5b47c787e96e04a0d2259348b05007b12832b298
bwissal <baazizwissal13@gmail.com>
2024-01-10 15:21:40 +0100

    add home controller

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -3,43 +3,14 @@
 namespace App\Controllers;
 
 use App\Controller;
-use App\Models\Journal;
+
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $journals = [
-            new Journal('My Third Journal Entry', '2023'),
-            new Journal('My Second Journal Entry', '2022'),
-            new Journal('My Second Journal', '2021')
-        ];
-
-        $this->render('index', ['journals' => $journals]);
+        $this->render('index');
     }
    
     
-    public function login()
-    {
-        
-        $this->render('login');
-    }
-    public function signup()
-    {
-        
-        $this->render('signup');
-    }
-    public function user()
-    {
-        
-        $this->render('user');
-    }
-    public function insert()
-    {
-        if ($_SERVER["REQUEST_METHOD"] === "POST") {
-            $name = isset($_POST["username"]) ? $_POST["username"] : "";
-            $email = isset($_POST["password"]) ? $_POST["password"] : "";
-            $this->render('layout/home', ['name' => $name]);
-        }
-    }
 }
