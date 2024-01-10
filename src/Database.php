b869305609d7fb4f5f75315463dcb28878479f04
bwissal <baazizwissal13@gmail.com>
2024-01-10 09:39:09 +0100

    add database conex

---
+++ src/Database.php
@@ -0,0 +1,43 @@
+<?php 
+namespace App;
+use PDOException;
+use PDO;
+class DatabaseConnection {
+    private static $instance;
+    private $connection;
+
+    private $host = 'localhost';
+    private $username = 'root';
+    private $password ='' ;
+    private $database ='wiki' ;
+
+    private function __construct() {
+        $this->connect();
+    }
+
+    public static function getInstance() {
+        if (!self::$instance) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    private function connect() {
+        try {
+            $dsn = "mysql:host={$this->host};dbname={$this->database}";
+            $this->connection = new PDO($dsn, $this->username, $this->password);
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die('Connection failed: ' . $e->getMessage());
+        }
+    }
+
+    public function getConnection() {
+        return $this->connection;
+    }
+
+    public function closeConnection() {
+        $this->connection = null;
+    }
+
+}
