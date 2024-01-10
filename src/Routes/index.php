509de55890e9f394f0a362baf22a1a98db92d3bc
bwissal <baazizwissal13@gmail.com>
2024-01-10 15:23:37 +0100

    add routing for login signup and

--- src/Routes/index.php
+++ src/Routes/index.php
@@ -1,16 +1,24 @@
 <?php
 
+use App\Controllers\Category;
+use App\Controllers\Dashboard;
 use App\Controllers\HomeController;
+use App\Controllers\LoginController;
+use App\Controllers\SignupController;
+use App\Controllers\TestController;
 use App\Router;
 
 $router = new Router();
 
 $router->get('/', HomeController::class, 'index');
-
-$router->get('/signup', HomeController::class, 'signup');
-$router->get('/login', HomeController::class, 'login');
+$router->get('/dashboard', Dashboard::class, 'index');
+$router->get('/category', Category::class, 'category');
+$router->get('/categoryadd', Category::class, 'categoryadd');
+$router->post('/categoryadd', Category::class, 'categoryadd');
 $router->get('/user', HomeController::class, 'user');
 $router->post('/insert', HomeController::class, 'insert');
 
-
+$router->get('/login', LoginController::class, 'show');
+$router->get('/signup', SignupController::class, 'show');
+$router->get('/test', TestController::class, 'add');
 $router->dispatch();
